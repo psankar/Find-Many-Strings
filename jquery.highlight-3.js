7f9e903f0c250924c4e51b438a62c313cb7bc8fa
Sankar P <sankar.curiosity@gmail.com>
2011-08-18 14:26:12 +0530

    Pass color as a parameter to the highlighter javascript to enable different colors for different strings

--- jquery.highlight-3.js
+++ jquery.highlight-3.js
@@ -14,14 +14,15 @@ Johann Burkard
 
 */
 
-jQuery.fn.highlight = function(pat) {
- function innerHighlight(node, pat) {
+jQuery.fn.highlight = function(pat, colo) {
+ function innerHighlight(node, pat, colo) {
   var skip = 0;
   if (node.nodeType == 3) {
    var pos = node.data.toUpperCase().indexOf(pat);
    if (pos >= 0) {
     var spannode = document.createElement('span');
     spannode.className = 'highlight';
+    spannode.setAttribute('style', colo);
     var middlebit = node.splitText(pos);
     var endbit = middlebit.splitText(pat.length);
     var middleclone = middlebit.cloneNode(true);
@@ -32,13 +33,13 @@ jQuery.fn.highlight = function(pat) {
   }
   else if (node.nodeType == 1 && node.childNodes && !/(script|style)/i.test(node.tagName)) {
    for (var i = 0; i < node.childNodes.length; ++i) {
-    i += innerHighlight(node.childNodes[i], pat);
+    i += innerHighlight(node.childNodes[i], pat, colo);
    }
   }
   return skip;
  }
  return this.each(function() {
-  innerHighlight(this, pat.toUpperCase());
+  innerHighlight(this, pat.toUpperCase(), colo);
  });
 };
 
